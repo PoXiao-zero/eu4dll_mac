1bf650641f35f4202126da352f60ef03508b718f
PoXiao-zero <271490940+PoXiao-zero@users.noreply.github.com>
2026-05-30 15:12:22 +0800

    feat: change east asian name order to surname-first

--- src/localization.cpp
+++ src/localization.cpp
@@ -5,6 +5,9 @@
 #include <CoreFoundation/CoreFoundation.h>
 
 namespace localization {
+
+    static bool g_is_Bimillennium_Universalis = false;
+
 /**
  修改函数：CTopbarGui::RefreshSpeedControlsWindow
  作用：将此函数使用的日期格式文本从"d w mw w y"修改为"y  mw d "
@@ -213,7 +216,13 @@ namespace localization {
 
         for (int i = 0; i < enabledMods->_nSize; ++i) {
             const std::string &modNameOrPath = enabledMods->_pData[i];
-            if (modNameOrPath.find("eu4_chinese.mod") != -1) {
+            if (modNameOrPath.find("eu4_chinese.mod") != -1 ||
+                modNameOrPath.find("Bimillennium_Universalis_") != -1) {
+
+                if (modNameOrPath.find("Bimillennium_Universalis_") != -1) {
+                    g_is_Bimillennium_Universalis = true;
+                }
+
                 auto patch = GetHostResourcePath();
                 if (patch.empty()) {
                     printf("加载字典资源路径失败！\n");
@@ -253,6 +262,186 @@ namespace localization {
         SET_SUCCESS();
     }
 
+    bool IsEastAsia(const std::string &cultureGroup_tag) {
+        if (!g_is_Bimillennium_Universalis) {
+            //中华 朝鲜 越南 日本 吐蕃 阿尔泰 鄂温克
+            static std::array<std::string, 7> cultures = {"east_asian", "korean_g", "southeastasian_group",
+                                                          "japanese_g",
+                                                          "tibetan_group", "altaic", "evenks",};
+            return std::find(cultures.begin(), cultures.end(), cultureGroup_tag) != cultures.end();
+        } else {
+            //风云世纪两千年MOD
+            //中华 朝鲜 日本 吐蕃 匈奴 鄂温克 东胡 钦察 扶余 百越 虾夷
+            static std::array<std::string, 11> cultures = {"east_asian", "korean_g", "japanese_g", "tibetan_group",
+                                                           "altaic", "evenks", "mongolic", "tartar", "buyeo_g",
+                                                           "thai_group", "ainu_g"};
+            return std::find(cultures.begin(), cultures.end(), cultureGroup_tag) != cultures.end();
+        }
+    }
+
+    bool IsChina(const std::string &cultureGroup_tag, const std::string &culture_tag) {
+        if (cultureGroup_tag != "east_asian" && culture_tag != "miao" && culture_tag != "bai" &&
+            culture_tag != "yi") {
+            return false;
+        }
+        return true;
+    }
+
+    void Monarch_GetFullName_Reversal(uintptr_t monarch, std::string *result, const std::string *lastName) {
+        uintptr_t culture = *(uintptr_t *) (monarch + 0x50);
+        auto *culture_tag = (std::string *) (culture + 0x38);
+        uintptr_t cultureGroup = *(uintptr_t *) (culture + 0x68);
+        auto *cultureGroup_tag = (std::string *) (cultureGroup + 0x38);
+//        printf("君主姓: %s 名: %s culture_tag: %s cultureGroup_tag: %s\n",
+//               strConvert::escapedStrToUtf8Val(*lastName).c_str(), strConvert::escapedStrToUtf8Val(*result).c_str(),
+//               culture_tag->c_str(), cultureGroup_tag->c_str());
+        if (!IsEastAsia(*cultureGroup_tag) && *culture_tag != "zhuang") {
+            result->append(" ");
+            result->append(*lastName);
+            return;
+        }
+        if (!IsChina(*cultureGroup_tag, *culture_tag)) {
+            //非中华文化组的在姓和名之间加上空格
+            result->insert(0, " ");
+        }
+        result->insert(0, *lastName);
+    }
+
+    extern "C" uintptr_t g_CMonarch_GetFullName_CallAddr = (uintptr_t) Monarch_GetFullName_Reversal;
+    extern "C" uintptr_t g_CMonarch_GetFullName_retAddr = 0;
+
+    __attribute__((naked)) void naked_CMonarch_GetFullName() {
+        __asm__ volatile (
+                ".intel_syntax noprefix \n"
+                "mov r15, [rbx+0x58] \n" // 君主家族信息偏移 CDynasty
+                "add r15, 8 \n" // CDynasty+8=家族姓
+
+                "mov rdi, rbx \n"// obj
+                "mov rsi, r14 \n"// 名
+                "mov rdx, r15 \n"// 姓
+                "call [rip + _g_CMonarch_GetFullName_CallAddr] \n"
+                "jmp [rip + _g_CMonarch_GetFullName_retAddr] \n"
+                ".att_syntax prefix \n"
+                );
+    }
+
+/**
+ Hook函数：CMonarch::GetFullName
+ 作用：HOOK君王获取名字函数，将东亚文化组的君王名字修改为姓在名前
+ */
+    void install_CMonarch_GetFullName() {
+        TRACK_FUNCTION();
+        uintptr_t matchAddress = ScanMainModule(
+                "4C 8B 7B 58 48 8D 35 ? ? ? ? 48 8D 7D D0 E8 ? ? ? ? 49 83 C7 08 48 8D 7D D0 4C 89 FE");
+        if (matchAddress == 0) {
+            printf("eu4dll_mac [Error] %s 特征码查找失败！\n", __func__);
+            return;
+        }
+        uintptr_t address = matchAddress;
+        g_CMonarch_GetFullName_retAddr = address + 0x3F;
+        HookJMP(address, (uintptr_t) naked_CMonarch_GetFullName);
+        printf("eu4dll_mac [Success] %s HookJMP 匹配地址:0x%lx 写入地址:0x%lx 返回地址:0x%lx\n",
+               __func__,
+               matchAddress, address, g_CMonarch_GetFullName_retAddr);
+        SET_SUCCESS();
+    }
+
+    void Country_GetNewRepublicName_Reversal(std::string *result, std::string *lastName, uintptr_t culture) {
+        auto *culture_tag = (std::string *) (culture + 0x38);
+        uintptr_t cultureGroup = *(uintptr_t *) (culture + 0x68);
+        auto *cultureGroup_tag = (std::string *) (cultureGroup + 0x38);
+
+//        printf("随机姓名_指定姓: %s %s 文化组：%s\n",
+//               strConvert::escapedStrToUtf8Val(*result).c_str(), strConvert::escapedStrToUtf8Val(*lastName).c_str(),
+//               cultureGroup_tag->c_str());
+
+        if (!IsEastAsia(*cultureGroup_tag) && *culture_tag != "zhuang") {
+            result->append(*lastName);
+            return;
+        }
+        result->pop_back();
+        if (!IsChina(*cultureGroup_tag, *culture_tag)) {
+            //非中华文化组的在姓和名之间加上空格
+            result->insert(0, " ");
+        }
+        result->insert(0, *lastName);
+    }
+
+    extern "C" uintptr_t g_CCountry_GetNewRepublicName_CallAddr = (uintptr_t) Country_GetNewRepublicName_Reversal;
+    extern "C" uintptr_t g_CCountry_GetNewRepublicName_RetAddr = 0;
+
+    __attribute__((naked)) void naked_CCountry_GetNewRepublicName() {
+        __asm__ volatile (
+                ".intel_syntax noprefix \n"
+
+                "mov rdx, [rbp-0x38] \n"// 文化
+                "call [rip + _g_CCountry_GetNewRepublicName_CallAddr] \n"
+                "jmp [rip + _g_CCountry_GetNewRepublicName_RetAddr] \n"
+                ".att_syntax prefix \n"
+                );
+    }
+
+/**
+ Hook函数：CCountry::GetNewRepublicName 指定姓分支
+ 作用：HOOK商人、外交官、将领等通用名字生成函数，将东亚文化组名字修改为姓在名前
+ */
+    void install_CCountry_GetNewRepublicName() {
+        TRACK_FUNCTION();
+        uintptr_t matchAddress = ScanMainModule(
+                "74 0D 4C 89 F7 E8 ? ? ? ? E9 93 00");
+        if (matchAddress == 0) {
+            printf("eu4dll_mac [Error] %s 特征码查找失败！\n", __func__);
+            return;
+        }
+        uintptr_t address = matchAddress + 5;
+        g_CCountry_GetNewRepublicName_RetAddr = address + 0x9D;
+        HookJMP(address, (uintptr_t) naked_CCountry_GetNewRepublicName);
+        printf("eu4dll_mac [Success] %s HookJMP 匹配地址:0x%lx 写入地址:0x%lx 返回地址:0x%lx\n",
+               __func__,
+               matchAddress, address, g_CCountry_GetNewRepublicName_RetAddr);
+        SET_SUCCESS();
+    }
+
+/**
+ Hook函数：CCountry::GetNewRepublicName 随机姓分支
+ 作用：HOOK商人、外交官、将领等通用名字生成函数，将东亚文化组名字修改为姓在名前
+ */
+    void install_CCountry_GetNewRepublicName_1() {
+        TRACK_FUNCTION();
+        uintptr_t matchAddress = ScanMainModule(
+                "48 8D 34 C1 4C 89 F7 E8 ? ? ? ? EB 5B");
+        if (matchAddress == 0) {
+            printf("eu4dll_mac [Error] %s 特征码查找失败！\n", __func__);
+            return;
+        }
+        uintptr_t address = matchAddress + 7;
+        HookJMP(address, (uintptr_t) naked_CCountry_GetNewRepublicName);
+        printf("eu4dll_mac [Success] %s HookJMP 匹配地址:0x%lx 写入地址:0x%lx 返回地址:0x%lx\n",
+               __func__,
+               matchAddress, address, g_CCountry_GetNewRepublicName_RetAddr);
+        SET_SUCCESS();
+    }
+
+/**
+ Hook函数：CCountry::GetNewRepublicName 文化姓分支
+ 作用：HOOK商人、外交官、将领等通用名字生成函数，将东亚文化组名字修改为姓在名前
+ */
+    void install_CCountry_GetNewRepublicName_2() {
+        TRACK_FUNCTION();
+        uintptr_t matchAddress = ScanMainModule(
+                "48 83 C0 08 4C 89 F7 48 89 C6 E8 ? ? ? ? 48 8D 7D 80");
+        if (matchAddress == 0) {
+            printf("eu4dll_mac [Error] %s 特征码查找失败！\n", __func__);
+            return;
+        }
+        uintptr_t address = matchAddress + 0xA;
+        HookJMP(address, (uintptr_t) naked_CCountry_GetNewRepublicName);
+        printf("eu4dll_mac [Success] %s HookJMP 匹配地址:0x%lx 写入地址:0x%lx 返回地址:0x%lx\n",
+               __func__,
+               matchAddress, address, g_CCountry_GetNewRepublicName_RetAddr);
+        SET_SUCCESS();
+    }
+
     void install() {
         //游戏界面右上角日期显示格式修改为年月日
         install_CTopbarGui_RefreshSpeedControlsWindow();
@@ -264,5 +453,11 @@ namespace localization {
 
         //根据是否加载汉化MOD来决定是否启用拼音搜索功能
         install_main();
+        //东亚君主名字反转
+        install_CMonarch_GetFullName();
+        //东亚随机生成名字反转
+        install_CCountry_GetNewRepublicName();
+        install_CCountry_GetNewRepublicName_1();
+        install_CCountry_GetNewRepublicName_2();
     }
 }
